f81c06235ca2c33a87fcc1ef74975b71bf1ade59
Niranjani-Mahavir <mahavir.niranjani@team.exelanceit.in>
2024-11-14 19:07:03 +0530

    sideNav in progress

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+import { navbarData } from './nav-data';
+import { flush } from '@angular/core/testing';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent {
+  collapsed = false;
+  navData = navbarData;
 
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+  }
+
+  closeSidenav(): void {
+    this.collapsed = false;
+  }
 }
